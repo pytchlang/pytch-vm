abb6515de286f787cc62eae1d8b3df7ee1e8fc57
Ben North <ben@redfrontdoor.org>
2019-09-19 21:15:56 +0100

    Implement keyboard interface via browser
    Will hook up to actual events in due course.

--- example/pytch/pytch-gui.js
+++ example/pytch/pytch-gui.js
@@ -24,6 +24,42 @@ $(document).ready(function() {
 
     ////////////////////////////////////////////////////////////////////////////////
     //
+    // Provide 'keyboard' interface via browser keyboard
+
+    const browser_keyboard = (() => {
+        let undrained_keydown_events = [];
+        let key_is_down = {};
+
+        const on_key_down = (e => {
+            key_is_down[e.key] = true;
+            undrained_keydown_events.push(e.key);
+            e.preventDefault();
+        });
+
+        const on_key_up = (e => {
+            key_is_down[e.key] = false;
+            e.preventDefault();
+        });
+
+        const drain_new_keydown_events = () => {
+            let evts = undrained_keydown_events;
+            undrained_keydown_events = [];
+            return evts;
+        };
+
+        const key_is_pressed = (keyname => (key_is_down[keyname] || false));
+
+        return {
+            on_key_down,
+            on_key_up,
+            key_is_pressed,
+            drain_new_keydown_events,
+        };
+    })();
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //
     // Connect Skulpt to our various interfaces
 
     Sk.configure({
