4588ec6da75db7e2b1937c1cbd35d3a007d9aa8d
Ben North <ben@redfrontdoor.org>
2019-10-27 11:45:38 +0000

    when_green_flag_clicked(): Add

--- src/lib/pytch/hat_blocks.py
+++ src/lib/pytch/hat_blocks.py
@@ -3,3 +3,7 @@ def _append_handler(fun, handler_type, handler_data=None):
         fun._pytch_handler_for = []
     fun._pytch_handler_for.append((handler_type, handler_data))
     return fun
+
+
+def when_green_flag_clicked(fun):
+    return _append_handler(fun, 'green-flag')
