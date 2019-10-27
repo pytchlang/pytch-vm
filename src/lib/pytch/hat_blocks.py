b33bcdc0ca7ed616a89d0794e6bfd40c85160a72
Ben North <ben@redfrontdoor.org>
2019-10-27 11:42:21 +0000

    hat_blocks: Add basis of module

---
+++ src/lib/pytch/hat_blocks.py
@@ -0,0 +1,5 @@
+def _append_handler(fun, handler_type, handler_data=None):
+    if not hasattr(fun, '_pytch_handler_for'):
+        fun._pytch_handler_for = []
+    fun._pytch_handler_for.append((handler_type, handler_data))
+    return fun
