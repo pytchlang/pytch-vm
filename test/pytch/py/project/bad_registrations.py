caf6bc7ceb85f27714588c771844f11946784bd6
Ben North <ben@redfrontdoor.org>
2019-10-27 12:44:23 +0000

    Add Py to test by-name lookup of Actor
    Includes duplicate registration of an Actor, which in due course we
    should forbid.

---
+++ test/pytch/py/project/bad_registrations.py
@@ -0,0 +1,15 @@
+from pytch import Project, Sprite
+
+
+class Alien(Sprite):
+    colour = 'green'
+
+
+class Banana(Sprite):
+    colour = 'yellow'
+
+
+project = Project()
+project.register_sprite_class(Alien)
+project.register_sprite_class(Alien)  # Leads to bad behaviour!
+project.register_sprite_class(Banana)
