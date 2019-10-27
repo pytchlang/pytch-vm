462138d90db39c6bc2bb927de06fa7b01924c939
Ben North <ben@redfrontdoor.org>
2019-10-27 11:29:13 +0000

    Test one instance created with correct attribute

--- test/pytch/test_project.js
+++ test/pytch/test_project.js
@@ -19,5 +19,11 @@ describe("pytch.project module", () => {
         let import_result = import_local_file("py/project/single_sprite.py");
         let project = import_result.$d.project.js_project;
         assert.strictEqual(project.actors.length, 1);
+
+        let actor_0 = project.actors[0];
+        assert.strictEqual(actor_0.instances.length, 1);
+
+        let instance_0 = actor_0.instances[0];
+        assert.strictEqual(instance_0.js_attr("n_clicks"), 0);
     });
 });
