9963e81d646020080a9cba74cf5780122a0d4d94
Ben North <ben@redfrontdoor.org>
2019-10-27 16:17:09 +0000

    Annotate broadcast test

--- test/pytch/test_scheduling.js
+++ test/pytch/test_scheduling.js
@@ -27,17 +27,27 @@ describe("scheduling", () => {
         let receiver = project.instance_0_by_class_name("Receiver");
         let sender = project.instance_0_by_class_name("Sender");
 
+        // Initially only the __init__() methods have run.
         assert.strictEqual(sender.js_attr("n_steps"), 0);
         assert.strictEqual(receiver.js_attr("n_events"), 0);
 
+        // Clicking green flag only launches the threads and puts them
+        // in the runnable queue.  Nothing has actually run yet.
         project.on_green_flag_clicked();
         assert.strictEqual(sender.js_attr("n_steps"), 0);
         assert.strictEqual(receiver.js_attr("n_events"), 0);
 
+        // First pass through scheduler causes an event in the sender,
+        // which notes a step, and broadcasts the message.
+        // Broadcasting the message only places a newly-created thread
+        // on the receiver in the run queue.  The receiver thread has
+        // not yet run.
         project.one_frame();
         assert.strictEqual(sender.js_attr("n_steps"), 1);
         assert.strictEqual(receiver.js_attr("n_events"), 0);
 
+        // Next pass through does give the receiver thread a go; and the
+        // sender continues to run.
         project.one_frame();
         assert.strictEqual(sender.js_attr("n_steps"), 2);
         assert.strictEqual(receiver.js_attr("n_events"), 1);
