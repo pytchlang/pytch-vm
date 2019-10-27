75115b6eae3653345fcc951a4797f2543a8b4781
Ben North <ben@redfrontdoor.org>
2019-10-27 16:14:23 +0000

    Test behaviour of broadcast sender too
    Also test state before green-flag clicked.

--- test/pytch/test_scheduling.js
+++ test/pytch/test_scheduling.js
@@ -25,13 +25,19 @@ describe("scheduling", () => {
         let project = import_result.$d.project.js_project;
 
         let receiver = project.instance_0_by_class_name("Receiver");
+        let sender = project.instance_0_by_class_name("Sender");
+
+        assert.strictEqual(sender.js_attr("n_steps"), 0);
+        assert.strictEqual(receiver.js_attr("n_events"), 0);
 
         project.on_green_flag_clicked();
 
         project.one_frame();
+        assert.strictEqual(sender.js_attr("n_steps"), 1);
         assert.strictEqual(receiver.js_attr("n_events"), 0);
 
         project.one_frame();
+        assert.strictEqual(sender.js_attr("n_steps"), 2);
         assert.strictEqual(receiver.js_attr("n_events"), 1);
     });
 });
