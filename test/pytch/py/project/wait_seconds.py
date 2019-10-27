34722fb1d8b5766b0143bbdbcbf070cd7de85013
Ben North <ben@redfrontdoor.org>
2019-10-27 17:27:53 +0000

    Add Python to test wait_seconds()

---
+++ test/pytch/py/project/wait_seconds.py
@@ -0,0 +1,21 @@
+import pytch
+from pytch import (
+    Project,
+    Sprite,
+    when_green_flag_clicked,
+)
+
+
+class Alien(Sprite):
+    def __init__(self):
+        self.n_steps = 0
+
+    @when_green_flag_clicked
+    def invade(self):
+        self.n_steps += 1
+        pytch.wait_seconds(0.25)
+        self.n_steps += 1
+
+
+project = Project()
+project.register_sprite_class(Alien)
