ff465b2aa4692816111202347327555f12f82e2d
Ben North <ben@redfrontdoor.org>
2019-11-02 08:11:32 +0000

    Add display properties to Sprite

--- src/lib/pytch/actor.py
+++ src/lib/pytch/actor.py
@@ -8,6 +8,13 @@ class Sprite(Actor):
          'library/images/question-mark.png', 16, 16),
     ]
 
+    def __init__(self):
+        self._x = 0
+        self._y = 0
+        self._size = 1.0
+        self._shown = False
+        self._appearance = 'question-mark';
+
 
 class Stage(Actor):
     Backdrops = []
