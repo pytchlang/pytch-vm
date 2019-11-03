80ddc4204d8dbebf9c08701c7eca03cbf6d6287e
Ben North <ben@redfrontdoor.org>
2019-11-03 08:54:08 +0000

    create_clone_of(): Add
    (As a partial placeholder.)

--- src/lib/pytch/clone.py
+++ src/lib/pytch/clone.py
@@ -2,6 +2,20 @@ import copy
 from pytch.syscalls import register_sprite_instance
 
 
+def create_clone_of(original):
+    """
+    Two variants, depending on whether the original is a class or an
+    instance.  If a class, we clone its instance-0.
+    """
+    if isinstance(original, type):
+        raise NotImplementedError('TODO: Clone instance-0 of a class')
+
+    # TODO: Use instance-0 of 'original' if 'original' is a class.
+    obj = original
+
+    return create_clone_of_instance(obj)
+
+
 def create_clone_of_instance(obj):
     new_obj = copy.deepcopy(obj)
     return register_sprite_instance(new_obj)
