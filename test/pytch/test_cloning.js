745569f8ec3d53956d9818d3ef6194934fdb54a3
Ben North <ben@redfrontdoor.org>
2019-11-03 09:15:56 +0000

    Test a chain of clone operations

--- test/pytch/test_cloning.js
+++ test/pytch/test_cloning.js
@@ -54,4 +54,50 @@ describe("cloning", () => {
         assert_all_attrs("copied_id", [42, 43, 43, 44]);
         assert_all_attrs("generated_id", [100, 101, 102, 103]);
     });
+
+    it("can chain-clone", async () => {
+        let import_result = await import_local_file("py/project/launch_clones.py");
+        let project = import_result.$d.project.js_project;
+        let broom_actor = project.actor_by_class_name("Broom");
+        let all_brooms = () => broom_actor.instances;
+
+        // Do not want to make assumptions about which order instances get
+        // cloned, so sort the returned list of values of attributes.
+        const assert_all_IDs = exp_values => {
+            let values = all_brooms().map(a => a.js_attr("copied_id"));
+            values.sort((x, y) => (x - y));
+            assert.deepStrictEqual(values, exp_values);
+        };
+
+        const frame_then_assert_all_IDs = exp_values => {
+            project.one_frame();
+            assert_all_IDs(exp_values);
+        };
+
+        // The synthetic broadcast just puts the handler threads in the queue;
+        // they don't run immediately.
+        project.do_synthetic_broadcast("clone-self");
+        assert_all_IDs([1])
+
+        // On the next frame the first clone is created, but the 'when start as
+        // clone' handlers do not yet run.
+        frame_then_assert_all_IDs([1, 1])
+
+        // On the next frame, the 'when cloned' handlers do run.  This
+        // increments the ID and creates another clone.
+        frame_then_assert_all_IDs([1, 2, 2])
+
+        // This repeats until we have five instances, with incrementing IDs.
+        frame_then_assert_all_IDs([1, 2, 3, 3])
+        frame_then_assert_all_IDs([1, 2, 3, 4, 4])
+        frame_then_assert_all_IDs([1, 2, 3, 4, 5])
+
+        // After the update of the fifth instance's ID to 5, all threads have
+        // run to completion.
+        assert.strictEqual(project.thread_groups.length, 0);
+
+        // Nothing should happen now.
+        for (let i = 0; i < 10; ++i)
+            frame_then_assert_all_IDs([1, 2, 3, 4, 5])
+    });
 });
