d08368ec4a96a75989b803b963bd93b222d6bca2
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2019-11-06 23:17:39 +0000

    astForPytchYield(): Add function
    Add function to create an AST node for a call to the Pytch syscall
    yield_until_next_frame().  When the Sk option 'pytchYield' is true,
    this gets patched into the AST at strategic points where we want a
    thread to yield.

--- src/ast.js
+++ src/ast.js
@@ -273,6 +273,24 @@ function new_identifier(n, c) {
     return Sk.builtin.str(n);
 }
 
+/*
+  This is the AST for a function call to pytch.yield_until_next_frame().  We
+  insert one of these at the end of every loop when we are in Pytch mode.  The
+  parameter 'n' is the tokeniser object for the outer loop.
+*/
+function astForPytchYield(n) {
+    var l = `'(auto-added yield at end of loop started on line ${n.lineno})'`;
+    var c = `'(auto-added yield at end of loop started on column ${n.col_offset})'`;
+    var attr = new Sk.astnodes.Attribute(new Sk.astnodes.Name(Sk.builtin.str("pytch"),
+                                                              Sk.astnodes.Load,
+                                                              l, c),
+                                         Sk.builtin.str("yield_until_next_frame"),
+                                         Sk.astnodes.Load,
+                                         l, c);
+    var call = new Sk.astnodes.Call(attr, null, null, l, c);
+    return new Sk.astnodes.Expr(call, l, c);
+}
+
 function astForCompOp (c, n) {
     /* comp_op: '<'|'>'|'=='|'>='|'<='|'!='|'in'|'not' 'in'|'is'
      |'is' 'not'
