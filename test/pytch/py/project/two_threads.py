cfe39afcfe1c6113a85ac9978d710146ca367f75
Glenn Strong <Glenn.Strong@scss.tcd.ie>
2019-11-06 23:29:29 +0000

    Add test Python code involving two threads

---
+++ test/pytch/py/project/two_threads.py
@@ -0,0 +1,35 @@
+import pytch
+from pytch import (
+    Sprite,
+    Project,
+    when_green_flag_clicked,
+)
+
+
+class T1(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.counter = 0
+
+    @when_green_flag_clicked
+    def start_counting(self):
+        self.counter = 0
+        while True:
+            self.counter += 1
+
+
+class T2(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.counter = 0
+
+    @when_green_flag_clicked
+    def start_counting(self):
+        self.counter = 0
+        while True:
+            self.counter += 1
+
+
+project = Project()
+project.register_sprite_class(T1)
+project.register_sprite_class(T2)
