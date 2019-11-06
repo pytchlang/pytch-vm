90cbd465aa132b4afffeb71348f1a04186c5b6ce
Ben North <ben@redfrontdoor.org>
2019-11-06 22:26:05 +0000

    (Refactor moving-ball tests)
    Hoist ball_at up a scope and use to simplify first (green-flag) test.

--- test/pytch/test_moving_ball.js
+++ test/pytch/test_moving_ball.js
@@ -1,6 +1,8 @@
 "use strict";
 
 describe("moving ball example", () => {
+    const ball_at = (x, y) => [["RenderImage", x, y, 1, "yellow-ball"]];
+
     it("renders correctly", async () => {
         let import_result = await import_local_file("py/project/moving_ball.py");
         let project = import_result.$d.project.js_project;
@@ -10,9 +12,7 @@ describe("moving ball example", () => {
         // Ball should go to (100, 50) but its costume has center
         // (8, 8) so, given different y sense of Stage and image,
         // it's rendered at (92, 58).
-        assert_renders_as("start",
-                          project,
-                          [["RenderImage", 92, 58, 1, "yellow-ball"]]);
+        assert_renders_as("start", project, ball_at(92, 58));
 
         // Set things going.
         project.on_green_flag_clicked();
@@ -20,25 +20,19 @@ describe("moving ball example", () => {
         // On the next rendered frame, it should have moved right
         // by 50.
         project.one_frame();
-        assert_renders_as("green-flag",
-                          project,
-                          [["RenderImage", 142, 58, 1, "yellow-ball"]]);
+        assert_renders_as("green-flag", project, ball_at(142, 58));
 
         // For 30 frames (the half-second sleep), it should not
         // move.  We have already done one of those 30, so for the
         // next 29 frames it should not move.
         for (let i = 0; i < 29; ++i) {
             project.one_frame();
-            assert_renders_as(`frame-${i}`,
-                              project,
-                              [["RenderImage", 142, 58, 1, "yellow-ball"]]);
+            assert_renders_as(`frame-${i}`, project, ball_at(142, 58));
         }
 
         // And now it should move another 60.
         project.one_frame();
-        assert_renders_as("frame-29",
-                          project,
-                          [["RenderImage", 202, 58, 1, "yellow-ball"]]);
+        assert_renders_as("frame-29", project, ball_at(202, 58));
 
         // Everything should have finished.
         assert.strictEqual(project.thread_groups.length, 0);
@@ -48,7 +42,6 @@ describe("moving ball example", () => {
         let import_result = await import_local_file("py/project/moving_ball.py");
         let project = import_result.$d.project.js_project;
 
-        const ball_at = (x, y) => [["RenderImage", x, y, 1, "yellow-ball"]];
         assert_renders_as("start", project, ball_at(92, 58));
 
         mock_keyboard.press_key('w')
