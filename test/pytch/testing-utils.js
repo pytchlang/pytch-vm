f18e961bf38586e1e795babbbad460816c3974cb
Ben North <ben@redfrontdoor.org>
2019-11-09 22:29:16 +0000

    Add yellow-banana, wooden to mock image size map

--- test/pytch/testing-utils.js
+++ test/pytch/testing-utils.js
@@ -98,6 +98,8 @@ before(() => {
         "library/images/square-80x80.png": [80, 80],
         "library/images/rectangle-60x30.png": [60, 30],
         "library/images/stage/solid-white.png": [480, 360],
+        "library/images/yellow-banana.png": [80, 30],
+        "library/images/stage/wooden.png": [480, 360],
     };
 
     class MockImage {
