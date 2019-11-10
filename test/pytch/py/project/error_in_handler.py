7d1df11444d56afae84d397dbe135401a994c123
Ben North <ben@redfrontdoor.org>
2019-11-10 07:45:54 +0000

    Add Python with error in method

---
+++ test/pytch/py/project/error_in_handler.py
@@ -0,0 +1,20 @@
+import pytch
+from pytch import (
+    Sprite,
+    Project,
+    when_I_receive,
+)
+
+
+class Alien(Sprite):
+    @when_I_receive('launch-invasion')
+    def do_something_bad(self):
+        self.engage_shields()
+
+    def engage_shields(self):
+        # No such method:
+        self.boost_shields()
+
+
+project = Project()
+project.register_sprite_class(Alien)
