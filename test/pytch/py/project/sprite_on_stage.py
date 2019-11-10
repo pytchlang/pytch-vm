46c19d80768ee9838e22e00b8b5d27f28a677f01
Ben North <ben@redfrontdoor.org>
2019-11-10 21:22:03 +0000

    Use 'when-clicked' handlers in test Python

--- test/pytch/py/project/sprite_on_stage.py
+++ test/pytch/py/project/sprite_on_stage.py
@@ -3,15 +3,25 @@ from pytch import (
     Stage,
     Sprite,
     Project,
+    when_this_sprite_clicked,
+    when_stage_clicked,
 )
 
 
 class Banana(Sprite):
     Costumes = [('yellow', 'library/images/yellow-banana.png', 50, 30)]
 
+    @when_this_sprite_clicked
+    def say_hello_banana(self):
+        print('hello from Banana')
+
 
 class Table(Stage):
     Backdrops = [('wooden', 'library/images/stage/wooden.png')]
+
+    @when_stage_clicked
+    def say_hello_table(self):
+        print('hello from Table')
 
 
 project = Project()
