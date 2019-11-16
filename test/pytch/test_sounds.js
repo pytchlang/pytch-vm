1f1ff1f111a14671185244dfd8527d3509c8f08c
Ben North <ben@redfrontdoor.org>
2019-11-16 07:19:44 +0000

    Add test-support functions for sounds

--- test/pytch/test_sounds.js
+++ test/pytch/test_sounds.js
@@ -5,4 +5,14 @@
 // Sounds
 
 describe("waiting and non-waiting sounds", () => {
+    let one_frame_fun = (project => () => {
+        mock_sound_manager.one_frame();
+        project.one_frame();
+    });
+
+    let assert_running_performances = (exp_tags => {
+        let got_tags = (mock_sound_manager.running_performances()
+                        .map(p => p.tag));
+        assert.deepStrictEqual(got_tags, exp_tags);
+    });
 });
