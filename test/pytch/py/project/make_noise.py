c3dea62027acc07b68fee202a25f7192961a9fcd
Ben North <ben@redfrontdoor.org>
2019-11-16 07:20:01 +0000

    Test behaviour of start_sound()

--- test/pytch/py/project/make_noise.py
+++ test/pytch/py/project/make_noise.py
@@ -10,6 +10,12 @@ class Orchestra(Sprite):
     Sounds = [('trumpet', 'library/sounds/trumpet.mp3'),
               ('violin', 'library/sounds/violin.mp3')]
 
+    @when_I_receive('play-trumpet')
+    def play_trumpet(self):
+        self.played_trumpet = 'no'
+        self.start_sound('trumpet')
+        self.played_trumpet = 'yes'
+
 
 project = Project()
 project.register_sprite_class(Orchestra)
--- test/pytch/test_sounds.js
+++ test/pytch/test_sounds.js
@@ -15,4 +15,36 @@ describe("waiting and non-waiting sounds", () => {
                         .map(p => p.tag));
         assert.deepStrictEqual(got_tags, exp_tags);
     });
+
+    it("can play trumpet", async () => {
+        let project = await import_project("py/project/make_noise.py");
+        let orchestra = project.instance_0_by_class_name("Orchestra");
+        let one_frame = one_frame_fun(project);
+
+        project.do_synthetic_broadcast("play-trumpet");
+
+        // On the next frame, the sound should start, but the launching
+        // thread shouldn't have run again yet.
+        one_frame();
+        assert_running_performances(["trumpet"]);
+        assert.strictEqual(orchestra.js_attr("played_trumpet"), "no")
+
+        // On the next frame, the sound should still be playing, and the
+        // launching thread will have run to completion.
+        one_frame();
+        assert_running_performances(["trumpet"]);
+        assert.strictEqual(orchestra.js_attr("played_trumpet"), "yes")
+        assert.strictEqual(project.thread_groups.length, 0);
+
+        // For the rest of the length of the 'trumpet' sound, it should stay
+        // playing.
+        for (let i = 0; i != 18; ++i) {
+            one_frame();
+            assert_running_performances(["trumpet"]);
+        }
+
+        // And then silence should fall again:
+        one_frame();
+        assert_running_performances([]);
+    });
 });
