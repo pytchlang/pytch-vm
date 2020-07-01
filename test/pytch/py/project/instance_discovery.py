77bef4d740b2e886343bb01970ec01cc0c216925
Ben North <ben@redfrontdoor.org>
2020-07-01 12:44:32 +0100

    Add test Python for instance discovery

---
+++ test/pytch/py/project/instance_discovery.py
@@ -0,0 +1,56 @@
+import pytch
+from pytch import (
+    Sprite,
+    Project,
+    when_green_flag_clicked,
+    when_I_start_as_a_clone,
+    when_I_receive,
+)
+
+
+G_next_global_id = 99
+def next_global_id():
+    global G_next_global_id
+    G_next_global_id += 1
+    return G_next_global_id
+
+
+class Alien(Sprite):
+    @when_green_flag_clicked
+    def init(self):
+        self.id = next_global_id()
+
+    @when_I_receive('make-clones')
+    def make_some_clones(self):
+        pytch.create_clone_of(self)
+        pytch.create_clone_of(self)
+        pytch.create_clone_of(self)
+
+    @when_I_start_as_a_clone
+    def set_id(self):
+        self.id = next_global_id()
+
+
+class Scanner(Sprite):
+    @when_green_flag_clicked
+    def init(self):
+        # Store the IDs rather than the actual Alien objects, because
+        # Sk.ffi.remapToJs() does not know how to handle Alien instances.
+        self.got_alien_ids = 0
+
+    @when_I_receive('get-original')
+    def get_original(self):
+        self.got_alien_ids = Alien.the_original().id
+
+    @when_I_receive('get-clones')
+    def get_clones(self):
+        self.got_alien_ids = sorted([a.id for a in Alien.all_clones()])
+
+    @when_I_receive('get-instances')
+    def get_instances(self):
+        self.got_alien_ids = sorted([a.id for a in Alien.all_instances()])
+
+
+project = Project()
+project.register_sprite_class(Alien)
+project.register_sprite_class(Scanner)
