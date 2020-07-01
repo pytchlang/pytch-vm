6dfaf8487f17278688e7e102340e2170e8fb34a9
Ben North <ben@redfrontdoor.org>
2020-07-01 12:47:51 +0100

    prepare_project(): Add test helper

--- test/pytch/test_instance_discovery.js
+++ test/pytch/test_instance_discovery.js
@@ -5,4 +5,12 @@
 // Discovery of instances: original, clones, all
 
 describe("instance discovery", () => {
+    const prepare_project = async () => {
+        let project = await import_project("py/project/instance_discovery.py");
+
+        project.on_green_flag_clicked();
+        project.one_frame();
+
+        return project;
+    };
 });
