5cccbbfe3608a8e866c537661a3dcee1f82ac3cc
Ben North <ben@redfrontdoor.org>
2020-07-01 12:49:33 +0100

    launch_clones(): Add test helper

--- test/pytch/test_instance_discovery.js
+++ test/pytch/test_instance_discovery.js
@@ -23,6 +23,15 @@ describe("instance discovery", () => {
         assert.deepEqual(got_ids, exp_ids);
     });
 
+    const launch_clones = (project => {
+        project.do_synthetic_broadcast('make-clones');
+        // Ensure enough frames go by for all create_clone_of() calls
+        // to run, and all clones' set_id() calls to run also:
+        project.one_frame();
+        project.one_frame();
+        project.one_frame();
+    });
+
     it("sets up the original Alien", async () => {
         let project = await prepare_project();
         assert_result(project, 'un-listened-for-message', 0);
