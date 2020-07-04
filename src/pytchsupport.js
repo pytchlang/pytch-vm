bf9b3b326c43eb97b6c2e890c94f137a010cae92
Ben North <ben@redfrontdoor.org>
2020-07-04 08:15:08 +0100

    pytch_in_module(): Add

--- src/pytchsupport.js
+++ src/pytchsupport.js
@@ -10,3 +10,18 @@ Sk.pytchsupport = Sk.pytchsupport || {};
  * Functions to support Pytch.
  *
  */
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Return the imported "pytch" module from the given object (in usage,
+ * this object will be a module).  Throw an error if no "pytch" found.
+ *
+ * TODO: Assert that "pytch" refers to a module?
+ */
+Sk.pytchsupport.pytch_in_module = (mod => {
+    if (mod.$d.hasOwnProperty('pytch'))
+        return mod.$d.pytch;
+    else
+        throw Error('module does not do "import pytch"');
+});
