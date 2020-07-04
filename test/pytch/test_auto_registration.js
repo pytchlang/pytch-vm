ed04a9f82f4e930f0246c77d62401529f1c1fc07
Ben North <ben@redfrontdoor.org>
2020-07-04 08:21:49 +0100

    pytch_in_module(): Test succeeding/failing

--- test/pytch/test_auto_registration.js
+++ test/pytch/test_auto_registration.js
@@ -13,4 +13,19 @@ configure_mocha();
 // Auto-registration helpers
 
 describe("auto-registration", () => {
+    with_module("py/project/go_live_empty_project.py", (import_module) => {
+        it("finds the imported pytch", async () => {
+            let module = await import_module();
+            let got_pytch = Sk.pytchsupport.pytch_in_module(module);
+            assert.ok(got_pytch);
+        });
+    });
+
+    with_module("py/project/no_import_pytch.py", (import_module) => {
+        it("detects lack of \"import pytch\"", async () => {
+            let module = await import_module();
+            assert.throws(() => Sk.pytchsupport.pytch_in_module(module),
+                          /does not do "import pytch"/);
+        });
+    });
 });
