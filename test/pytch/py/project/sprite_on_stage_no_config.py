9cda170a17121cde1bf0c62d3281d1479aaa0a5f
Ben North <ben@redfrontdoor.org>
2020-07-04 09:01:07 +0100

    Add test Python without Project boilerplate

---
+++ test/pytch/py/project/sprite_on_stage_no_config.py
@@ -0,0 +1,24 @@
+import pytch
+from pytch import (
+    Stage,
+    Sprite,
+    Project,
+    when_this_sprite_clicked,
+    when_stage_clicked,
+)
+
+
+class Banana(Sprite):
+    Costumes = [('yellow', 'library/images/yellow-banana.png', 50, 30)]
+
+    @when_this_sprite_clicked
+    def say_hello_banana(self):
+        print('hello from Banana')
+
+
+class Table(Stage):
+    Backdrops = [('wooden', 'library/images/stage/wooden.png')]
+
+    @when_stage_clicked
+    def say_hello_table(self):
+        print('hello from Table')
