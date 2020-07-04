063389bc0e9c9448004ed6fc7f9124c6d057641e
Ben North <ben@redfrontdoor.org>
2020-07-04 08:37:56 +0100

    actors_of_module(): Add

--- src/pytchsupport.js
+++ src/pytchsupport.js
@@ -27,10 +27,39 @@ Sk.pytchsupport.pytch_in_module = (mod => {
 });
 
 
+/**
+* Return a list of the actors (Sprites or Stages) in the given module.
+* Each entry in the list is an object of the form
+*
+* { cls: Python class object,
+*   kind: JS string "Sprite" or "Stage" }
+*/
+Sk.pytchsupport.actors_of_module = (mod => {
+    const pytch = Sk.pytchsupport.pytch_in_module(mod);
+    const pytch_Sprite = pytch.$d.Sprite;
+    const pytch_Stage = pytch.$d.Stage;
+
+    const is_strict_subclass = (obj, cls) => (
+        (obj.sk$klass && Sk.builtin.issubclass(obj, cls) && obj !== cls));
+
+    let actors = [];
+
+    Object.values(mod.$d).forEach(attr => {
+        if (is_strict_subclass(attr, pytch_Sprite))
+            actors.push({ cls: attr, kind: "Sprite" });
+        if (is_strict_subclass(attr, pytch_Stage))
+            actors.push({ cls: attr, kind: "Stage" });
+    });
+
+    return actors;
+});
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
 [
     "pytch_in_module",
+    "actors_of_module",
 ].forEach(
     fun_name => {
         Sk.exportSymbol(`Sk.pytchsupport.${fun_name}`, Sk.pytchsupport[fun_name]);
