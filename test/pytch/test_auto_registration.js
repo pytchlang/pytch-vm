3294d20a3cb1629b7b59154be86ad7fa42e4621a
Ben North <ben@redfrontdoor.org>
2020-07-04 08:21:26 +0100

    Add test file skeleton

---
+++ test/pytch/test_auto_registration.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {
+    configure_mocha,
+    with_module,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Auto-registration helpers
+
+describe("auto-registration", () => {
+});
