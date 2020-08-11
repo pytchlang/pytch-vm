552e53cdf37c9eed427ab8ed5cb62c4fca7ab564
Ben North <ben@redfrontdoor.org>
2020-08-11 17:19:01 +0100

    Add test Python for Sprite with unknown Sound

---
+++ test/pytch/py/project/bad_sound.py
@@ -0,0 +1,14 @@
+from pytch import Project, Sprite
+
+
+class Alien(Sprite):
+    Sounds = [('xylophone', 'library/sounds/no-such-instrument.mp3')]
+
+
+project = Project()
+
+caught_exception = None
+try:
+    project.register_sprite_class(Alien)
+except Exception as error:
+    caught_exception = error
