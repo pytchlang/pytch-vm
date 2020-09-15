1cac36a15125ede76ea26e0451e5184203cf34c9
Ben North <ben@redfrontdoor.org>
2020-09-15 09:37:36 +0100

    Throw SyntaxError if no "import pytch"
    Not truly an actual "Syntax" error, but close I think.

--- src/pytchsupport.js
+++ src/pytchsupport.js
@@ -15,7 +15,7 @@ Sk.pytchsupport = Sk.pytchsupport || {};
 
 /**
  * Return the imported "pytch" module from the given object (in usage, this
- * object will be a module).  Throw an error if no "pytch" found.
+ * object will be a module).  Throw a Skulpt SyntaxError if no "pytch" found.
  *
  * TODO: Assert that "pytch" refers to a module?
  */
@@ -23,7 +23,7 @@ Sk.pytchsupport.pytch_in_module = (mod => {
     if (mod.$d.hasOwnProperty('pytch'))
         return mod.$d.pytch;
     else
-        throw Error('module does not do "import pytch"');
+        throw new Sk.builtin.SyntaxError('module does not do "import pytch"');
 });
 
 
