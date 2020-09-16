18d56bd8083d952907419248ec3f4ab541c8aac3
Ben North <ben@redfrontdoor.org>
2020-09-16 13:46:30 +0100

    assertBuildError(): Add test helper

--- test/pytch/test_build_errors.js
+++ test/pytch/test_build_errors.js
@@ -11,5 +11,23 @@ configure_mocha();
 //
 // Build-error handling.
 
+const assertBuildError = (err, exp_phase, innerMsgRegExp) => {
+    const msg = Sk.builtin.str(err).v;
+    assert.ok(
+        /^PytchBuildError/.test(msg),
+        `did not get PytchBuildError: ${msg}`
+    );
+    assert.equal(err.phase, exp_phase);
+
+    if (innerMsgRegExp != null) {
+        const innerMsg = Sk.builtin.str(err.innerError).v;
+        assert.ok(
+            innerMsgRegExp.test(innerMsg),
+            (`innerError message "${innerMsg}"`
+             + ` did not match /${innerMsgRegExp.source}/`)
+        );
+    }
+};
+
 describe("build-error handling", () => {
 });
