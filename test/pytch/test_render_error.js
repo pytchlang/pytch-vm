9312321e91af7ea1d8bf54aa6c53f134c67a4836
Ben North <ben@redfrontdoor.org>
2020-09-16 09:04:32 +0100

    Test for expected retval and error on render
    Currently fails; will fix shortly.

--- test/pytch/test_render_error.js
+++ test/pytch/test_render_error.js
@@ -3,6 +3,8 @@
 const {
     configure_mocha,
     with_project,
+    assert,
+    pytch_errors,
 } = require("./pytch-testing.js");
 configure_mocha();
 
@@ -16,6 +18,13 @@ describe("rendering error-handler", () => {
     with_project("py/project/render_error.py", (import_project) => {
         it("passes error to callback", async () => {
             const project = await import_project();
+            project.on_green_flag_clicked();
+            project.one_frame();
+            assert.strictEqual(project.rendering_instructions(), null);
+            const thrown_errors = pytch_errors.drain_errors();
+            assert.equal(thrown_errors.length, 1);
+            const error_str = thrown_errors[0].err.toString();
+            assert.ok(/'Problem' object has no attribute/.test(error_str));
         });
     });
 });
