2d54fa616ac066ea8ec6f11cb2502f8082a32431
Ben North <ben@redfrontdoor.org>
2020-09-16 13:46:13 +0100

    Add test skeleton for build errors

---
+++ test/pytch/test_build_errors.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {
+    configure_mocha,
+    with_project,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Build-error handling.
+
+describe("build-error handling", () => {
+});
