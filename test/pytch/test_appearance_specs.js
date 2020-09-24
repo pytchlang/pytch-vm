0ffef9b4771ae9f6f99c29a0c3bc31f9118f72fb
Ben North <ben@redfrontdoor.org>
2020-09-24 20:25:38 +0100

    Add skeleton test file

---
+++ test/pytch/test_appearance_specs.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {
+    configure_mocha,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Different ways of specifying a Costume or Backdrop
+
+describe("Costume spec parsing", () => {
+});
