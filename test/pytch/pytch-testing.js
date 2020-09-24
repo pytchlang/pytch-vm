a65aa7e361956aba2b83918221a8292d73565026
Ben North <ben@redfrontdoor.org>
2020-09-24 17:24:23 +0100

    Tests: Remove "user-projects/1234" mock sounds

--- test/pytch/pytch-testing.js
+++ test/pytch/pytch-testing.js
@@ -272,8 +272,6 @@ class MockImage {
 const sound_duration_from_url = new Map([
     ["project-assets/library/sounds/trumpet.mp3", 20],
     ["project-assets/library/sounds/violin.mp3", 10],
-    ["user-projects/1234/project-assets/library/sounds/trumpet.mp3", 43],
-    ["user-projects/1234/project-assets/library/sounds/violin.mp3", 30],
 ]);
 
 class MockSound {
