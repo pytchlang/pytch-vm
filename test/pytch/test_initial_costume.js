ef364ad035ce773b4ff55554990e49669dc560b0
Ben North <ben@redfrontdoor.org>
2020-09-25 21:33:54 +0100

    Test Sprite with Costume is initially shown

--- test/pytch/test_initial_costume.js
+++ test/pytch/test_initial_costume.js
@@ -13,4 +13,17 @@ configure_mocha();
 // Automatic switch_costume() and show() if Sprite has at least one Costume
 
 describe("Automatic switch-costume/show", () => {
+    it("starts off showing a Sprite with Costume", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            class Banana(pytch.Sprite):
+                Costumes = ["yellow-banana.png"]
+        `);
+
+        // Costume is 80 x 30 so with the auto-centre behaviour and y-flip, we
+        // expect the rendering at (-40, 15).
+        assert_renders_as("startup", project,
+                          [["RenderImage", -40, 15, 1, "yellow-banana"]]);
+    });
 });
