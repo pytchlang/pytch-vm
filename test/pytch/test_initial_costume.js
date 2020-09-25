085b4a8a3432e320701f434d09cf3773bdc49f37
Ben North <ben@redfrontdoor.org>
2020-09-25 21:33:34 +0100

    Tests: Add skeleton for initial costume choice

---
+++ test/pytch/test_initial_costume.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    assert_renders_as,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Automatic switch_costume() and show() if Sprite has at least one Costume
+
+describe("Automatic switch-costume/show", () => {
+});
