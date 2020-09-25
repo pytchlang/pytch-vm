fe43826af30e830757086074552be6dbcf0ade1e
Ben North <ben@redfrontdoor.org>
2020-09-25 10:49:27 +0100

    Define test case for existing parser
    The existing 'parser' requires the full set of four components.

--- test/pytch/test_appearance_specs.js
+++ test/pytch/test_appearance_specs.js
@@ -12,4 +12,16 @@ configure_mocha();
 // Different ways of specifying a Costume or Backdrop
 
 describe("Costume spec parsing", () => {
+    const test_cases = [
+        {
+            label: "full four-component",
+            fragment: '("yellow", "library/images/yellow-banana.png", 25, 20)',
+            exp_info: [
+                "yellow",
+                "project-assets/library/images/yellow-banana.png",
+                80, 30,
+                25, 20,
+            ],
+        },
+    ];
 });
