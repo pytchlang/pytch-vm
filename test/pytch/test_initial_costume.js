e2ab19b382cddea0ba0088ccba839ca3cc63138b
Ben North <ben@redfrontdoor.org>
2020-09-25 21:37:41 +0100

    Tests: Explicitly check "_shown" attribute value

--- test/pytch/test_initial_costume.js
+++ test/pytch/test_initial_costume.js
@@ -3,6 +3,8 @@
 const {
     configure_mocha,
     import_deindented,
+    js_getattr,
+    assert,
     assert_renders_as,
 } = require("./pytch-testing.js");
 configure_mocha();
@@ -21,6 +23,10 @@ describe("Automatic switch-costume/show", () => {
                 Costumes = ["yellow-banana.png"]
         `);
 
+        const banana_0 = project.instance_0_by_class_name("Banana");
+        const shown = js_getattr(banana_0.py_object, "_shown");
+        assert.equal(shown, true);
+
         // Costume is 80 x 30 so with the auto-centre behaviour and y-flip, we
         // expect the rendering at (-40, 15).
         assert_renders_as("startup", project,
@@ -34,6 +40,10 @@ describe("Automatic switch-costume/show", () => {
             class Monitor(pytch.Sprite):
                 Costumes = []
         `);
+
+        const monitor_0 = project.instance_0_by_class_name("Monitor");
+        const shown = js_getattr(monitor_0.py_object, "_shown");
+        assert.equal(shown, false);
 
         assert_renders_as("startup", project, []);
     });
