ec00a041182c2459ea213b8da5f6a29bbbbc272e
Ben North <ben@redfrontdoor.org>
2020-09-26 12:31:18 +0100

    Add test Python code

--- test/pytch/test_speech_bubbles.js
+++ test/pytch/test_speech_bubbles.js
@@ -14,5 +14,23 @@ configure_mocha();
 
 describe("Speech bubbles", () => {
     it("includes speech-bubble instructions", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            class Banana(pytch.Sprite):
+                Costumes = ["yellow-banana.png"]
+
+                @pytch.when_I_receive("talk")
+                def talk(self):
+                    self.say("Hello world")
+
+                @pytch.when_I_receive("silence")
+                def fall_silent(self):
+                    self.say_nothing()
+
+                @pytch.when_I_receive("talk-briefly")
+                def talk_briefly(self):
+                    self.say_for_seconds("Mumble", 0.5)
+        `);
     });
 });
