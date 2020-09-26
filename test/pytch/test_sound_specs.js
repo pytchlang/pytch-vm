9eb82b790e0353cf16dd337bd52c10d9719a2964
Ben North <ben@redfrontdoor.org>
2020-09-26 15:54:29 +0100

    Tests: Add skeleton for parsing Sound specs

---
+++ test/pytch/test_sound_specs.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Different ways of specifying a Sound
+
+describe("Sound spec parsing", () => {
+});
