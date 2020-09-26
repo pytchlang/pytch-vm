a3de74a345e9d359009836540d82afc3f1d48701
Ben North <ben@redfrontdoor.org>
2020-09-26 12:12:10 +0100

    Tests: Add skeleton test file

---
+++ test/pytch/test_speech_bubbles.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    assert_renders_as,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Speech bubbles
+
+describe("Speech bubbles", () => {
+    it("includes speech-bubble instructions", async () => {
+    });
+});
