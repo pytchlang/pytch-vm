0aad188ccd292877c61a67737d8bcd224ac7c970
Ben North <ben@redfrontdoor.org>
2020-09-26 15:54:52 +0100

    Tests: Define 'good' cases

--- test/pytch/test_sound_specs.js
+++ test/pytch/test_sound_specs.js
@@ -13,4 +13,21 @@ configure_mocha();
 // Different ways of specifying a Sound
 
 describe("Sound spec parsing", () => {
+    const good_cases = [
+        {
+            label: "full two-component",
+            fragment: '("toot", "trumpet.mp3")',
+            exp_info: ["toot", "trumpet.mp3"],
+        },
+        {
+            label: "filename as 1-elt tuple; infer label",
+            fragment: '("trumpet.mp3",)',
+            exp_info: ["trumpet", "trumpet.mp3"],
+        },
+        {
+            label: "filename as bare string; infer label",
+            fragment: '"trumpet.mp3"',
+            exp_info: ["trumpet", "trumpet.mp3"],
+        },
+    ];
 });
