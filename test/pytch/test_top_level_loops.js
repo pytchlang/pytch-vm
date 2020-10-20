71271b0c3d27bfd346882f4926ba7f67282674ab
Ben North <ben@redfrontdoor.org>
2020-10-20 11:50:39 +0100

    Add test skeleton for top-level for/while loops

---
+++ test/pytch/test_top_level_loops.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    js_getattr,
+    one_frame,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Use of 'for' and 'while' loops at top level of module
+
+describe("Use of for/while loops at module top-level", () => {
+});
