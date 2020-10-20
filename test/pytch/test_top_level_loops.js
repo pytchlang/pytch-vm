f9687809e189738bd29f6ea19de0654b38f023f9
Ben North <ben@redfrontdoor.org>
2020-10-20 11:57:45 +0100

    Test behaviour of top-level loops
    Currently fails with build error; will fix shortly.

--- test/pytch/test_top_level_loops.js
+++ test/pytch/test_top_level_loops.js
@@ -32,4 +32,29 @@ describe("Use of for/while loops at module top-level", () => {
         { label: 'while loop', code: while_loop_code },
     ];
 
+    loop_test_specs.forEach(spec =>
+        it(`allows top-level loop (${spec.label})`, async () => {
+            const project = await import_deindented(`
+
+                import pytch
+
+                global_x = 0
+                ${spec.code}
+
+                class Banana(pytch.Sprite):
+                    Costumes = []
+
+                    @pytch.when_I_receive("read-x")
+                    def read_x(self):
+                        self.x = global_x
+            `);
+
+            const banana_0 = project.instance_0_by_class_name("Banana");
+            const current_x = () => js_getattr(banana_0.py_object, "x");
+
+            project.do_synthetic_broadcast("read-x");
+            one_frame(project);
+            assert.strictEqual(current_x(), 0 + 1 + 2 + 3 + 4);
+        })
+    );
 });
