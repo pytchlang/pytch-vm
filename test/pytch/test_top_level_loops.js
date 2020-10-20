1e4549a747bab607e03f41948e8735b4c86a4391
Ben North <ben@redfrontdoor.org>
2020-10-20 11:57:12 +0100

    Define for/while code for use at top level

--- test/pytch/test_top_level_loops.js
+++ test/pytch/test_top_level_loops.js
@@ -15,4 +15,16 @@ configure_mocha();
 // Use of 'for' and 'while' loops at top level of module
 
 describe("Use of for/while loops at module top-level", () => {
+    const for_loop_code = `# Use for loop
+                for i in range(5):
+                    global_x += i
+    `;
+
+    const while_loop_code = `# Use while loop
+                i = 0
+                while i < 5:
+                    global_x += i
+                    i += 1
+    `;
+
 });
