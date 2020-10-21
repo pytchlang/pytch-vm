7adf96fecbd6b128bddd4b6143fe7c4c7446b2e7
Ben North <ben@redfrontdoor.org>
2020-10-21 13:51:45 +0100

    Add test skeleton for "continue" in loops

---
+++ test/pytch/test_continue_in_loops.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    js_getattr,
+    one_frame,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Use of "continue" in for and while loops
+
+describe("Use of 'continue' in loops", () => {
+});
