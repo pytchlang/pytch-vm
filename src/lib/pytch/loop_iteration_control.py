4eced904851431eead2743d93ee505f2788048e2
Ben North <ben@redfrontdoor.org>
2020-10-22 12:42:25 +0100

    multiple_loop_iterations_decorator: Add

--- src/lib/pytch/loop_iteration_control.py
+++ src/lib/pytch/loop_iteration_control.py
@@ -14,3 +14,12 @@ class LoopIterationsPerFrame:
     def __exit__(self, exc_type, exc_value, traceback):
         # Behave the same whether leaving normally or through exception.
         pop_loop_iterations_per_frame()
+
+
+def multiple_loop_iterations_decorator(n_iters):
+    def decorator(fun):
+        def decorated_fun(*args, **kwargs):
+            with LoopIterationsPerFrame(n_iters):
+                return fun(*args, **kwargs)
+        return decorated_fun
+    return decorator
