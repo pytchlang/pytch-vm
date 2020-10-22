9f0f676ad8ce7232b57ed31c42de578cadc82ab6
Ben North <ben@redfrontdoor.org>
2020-10-22 12:42:04 +0100

    LoopIterationsPerFrame: Add context-manager class

--- src/lib/pytch/loop_iteration_control.py
+++ src/lib/pytch/loop_iteration_control.py
@@ -2,3 +2,15 @@ from .syscalls import (
     push_loop_iterations_per_frame,
     pop_loop_iterations_per_frame,
 )
+
+
+class LoopIterationsPerFrame:
+    def __init__(self, n_iterations_per_frame):
+        self.n_iterations_per_frame = n_iterations_per_frame
+
+    def __enter__(self):
+        push_loop_iterations_per_frame(self.n_iterations_per_frame)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Behave the same whether leaving normally or through exception.
+        pop_loop_iterations_per_frame()
