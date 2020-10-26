1bf102ab03487a60b246f46127e31f572702580e
Ben North <ben@redfrontdoor.org>
2020-10-26 07:33:07 +0000

    Add test skeleton for Python 3 features

---
+++ test/pytch/test_python3.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Python 3 features
+
+describe("support for Python 3", () => {
+});
