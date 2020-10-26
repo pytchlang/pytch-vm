6292981d15f7786199269c6f6921dc2e555c90dd
Ben North <ben@redfrontdoor.org>
2020-10-26 07:33:18 +0000

    Add "can work with f-strings" test

--- test/pytch/test_python3.js
+++ test/pytch/test_python3.js
@@ -3,6 +3,7 @@
 const {
     configure_mocha,
     import_deindented,
+    pytch_stdout,
     assert,
 } = require("./pytch-testing.js");
 configure_mocha();
@@ -12,4 +13,17 @@ configure_mocha();
 // Python 3 features
 
 describe("support for Python 3", () => {
+    it("can work with f-strings", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            greeting = "Hello"
+
+            # Error under Python 2:
+            print(f"{greeting} world")
+        `);
+
+        const stdout = pytch_stdout.drain_stdout();
+        assert.equal(stdout, "Hello world\n");
+    });
 });
