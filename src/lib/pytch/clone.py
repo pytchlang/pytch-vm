8595150f70a6ef3371583195bf445a91d5e6fa02
Ben North <ben@redfrontdoor.org>
2020-11-09 17:15:33 +0000

    Rename variable

--- src/lib/pytch/clone.py
+++ src/lib/pytch/clone.py
@@ -2,23 +2,23 @@ import copy
 from pytch.syscalls import register_sprite_instance
 
 
-def create_clone_of(original):
+def create_clone_of(original_cls_or_obj):
     """
     Two variants, depending on whether the original is a class or an
     instance.  If a class, we clone its instance-0.
     """
-    if isinstance(original, type):
-        if not hasattr(original, "_pytch_parent_project"):
+    if isinstance(original_cls_or_obj, type):
+        if not hasattr(original_cls_or_obj, "_pytch_parent_project"):
             raise ValueError("can only clone a Pytch-registered class")
 
         # Would be surprising if this fails, but handle anyway.
         try:
-            obj = original.the_original()
+            obj = original_cls_or_obj.the_original()
         except:
             raise RuntimeError("the_original() failed")
 
     else:
-        obj = original
+        obj = original_cls_or_obj
 
     return create_clone_of_instance(obj)
 
