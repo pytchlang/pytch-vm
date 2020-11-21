09e5fc64e9b2e93fed41e6b278c957aa7e2ff268
Ben North <ben@redfrontdoor.org>
2020-11-21 07:54:25 +0000

    Add test Python

--- test/pytch/test_glide.js
+++ test/pytch/test_glide.js
@@ -15,6 +15,16 @@ configure_mocha();
 
 describe("Behaviour of glide-to method", () => {
     it("executes glide", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            class Banana(pytch.Sprite):
+                Costumes = ["yellow-banana.png"]
+                @pytch.when_I_receive("run")
+                def slide_across_screen(self):
+                    self.go_to_xy(-120, -120)
+                    self.glide_to_xy(0, 120, 1.0)
+        `);
     });
 });
 
