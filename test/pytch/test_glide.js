c959711ed5730cd68060341e2eb5384a28f89389
Ben North <ben@redfrontdoor.org>
2020-11-21 07:42:14 +0000

    Add test skeleton

---
+++ test/pytch/test_glide.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    one_frame,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Glide block
+
+describe("Behaviour of glide-to method", () => {
+    it("executes glide", async () => {
+    });
+});
+
+
