2a496272d2f89603087a9a39a16da68de6f30d55
Ben North <ben@redfrontdoor.org>
2020-12-03 15:49:04 +0000

    Add script to make website layer zipfile
    Largely copied from existing script in pytch-build repo.

---
+++ website-layer/make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BUILD_DIR="$(realpath "$(dirname "$0")")"
+REPO_ROOT="$(realpath "$BUILD_DIR"/..)"
+
+cd "$REPO_ROOT"
+
+LAYER_DIR=website-layer/layer-content
+SKULPT_DIR="$LAYER_DIR"/skulpt
+
+if [ -e node_modules -o -e $LAYER_DIR ]; then
+    echo "Must be run in a clean clone"
+    echo '(i.e., no "node_modules" or "'"$LAYER_DIR"'")'
+    exit 1
+fi
+
+npm install
+npm run build
+
+mkdir -p "$SKULPT_DIR"
+
+(
+    cd dist
+    cp --target-directory=../"$SKULPT_DIR" \
+           skulpt.min.js \
+           skulpt.min.js.map \
+           skulpt-stdlib.js
+)
+
+(
+    cd "$LAYER_DIR"
+    find skulpt -type d -print0 | xargs -0 chmod 755
+    find skulpt -type f -print0 | xargs -0 chmod 644
+    zip -r ../layer.zip skulpt
+)
