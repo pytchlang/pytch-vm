42e5f35c9db68c73de4244b387b408fca8f57d52
Ben North <ben@redfrontdoor.org>
2021-01-24 10:12:33 +0000

    Add test skeleton for docstrings

---
+++ test/pytch/test_docstrings.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    one_frame,
+    pytch_stdout,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Docstrings
+
+describe("Docstrings", () => {
+});
