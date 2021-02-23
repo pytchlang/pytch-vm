5b22dc12a5665e2ead312060ffa91d77d6308836
Ben North <ben@redfrontdoor.org>
2021-02-23 08:33:41 +0000

    Add skeleton test for docstring / completion info

---
+++ test/pytch/test_completions_info.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {
+    configure_mocha,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Extraction of completion info
+
+describe("Completion info extraction", () => {
+});
