8a8d96362c06df67caae80d77ab191f4b913dd18
Ben North <ben@redfrontdoor.org>
2021-02-23 08:34:20 +0000

    _parse_raw_doc(): Test

--- test/pytch/test_completions_info.js
+++ test/pytch/test_completions_info.js
@@ -12,4 +12,48 @@ configure_mocha();
 // Extraction of completion info
 
 describe("Completion info extraction", () => {
+    describe("parses raw docstrings correctly", () => {
+        const mod = Sk.builtin.__import__("pytch._completions_info", {}, {}, [], -1);
+
+        const parseFun = Sk.builtin.getattr(
+            Sk.builtin.getattr(
+                mod,
+                new Sk.builtin.str("_completions_info")
+            ),
+            new Sk.builtin.str("_parse_raw_doc")
+        );
+
+        const parse = (jsString) => {
+            const pyString = new Sk.builtin.str(jsString);
+            const pyResult = parseFun.tp$call([pyString], {});
+            return {
+                suffix: pyResult.v[0].v,
+                doc: pyResult.v[1].v,
+            };
+        };
+
+        it("without suffix", () => {
+            const { suffix, doc } = parse("Hello world");
+            assert.equal(suffix, "");
+            assert.equal(doc, "Hello world");
+        });
+
+        it("with suffix", () => {
+            const { suffix, doc } = parse("(GREETING) Hello");
+            assert.equal(suffix, "(GREETING)");
+            assert.equal(doc, "Hello");
+        });
+
+        it("empty-parentheses suffix", () => {
+            const { suffix, doc } = parse("() Hello");
+            assert.equal(suffix, "()");
+            assert.equal(doc, "Hello");
+        });
+
+        it("multiline", () => {
+            const { suffix, doc } = parse("Hello\nworld");
+            assert.equal(suffix, "");
+            assert.equal(doc, "Hello");
+        });
+    });
 });
