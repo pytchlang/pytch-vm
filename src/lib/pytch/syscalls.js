2580d8f35f462e94061d951a736ff756dd8aa5d8
Ben North <ben@redfrontdoor.org>
2021-05-07 11:15:58 +0100

    new_pytch_suspension(): Add set_success/failure()
    Add methods
    
        set_success()
        set_failure()
    
    on the 'data' property of a Pytch suspension.

--- src/lib/pytch/syscalls.js
+++ src/lib/pytch/syscalls.js
@@ -6,15 +6,24 @@ var $builtinmodule = function (name) {
     const new_pytch_suspension = (syscall_name, syscall_args) => {
         let susp = new Sk.misceval.Suspension();
 
-        susp.data = {
-            type: "Pytch",
-            subtype: syscall_name,
-            subtype_data: syscall_args,
-            result: {  // Default is for a syscall to return None
-                kind: "success",
-                value: Sk.builtin.none.none$,
-            },
-        };
+        susp.data = (() => {
+            let data = {
+                type: "Pytch",
+                subtype: syscall_name,
+                subtype_data: syscall_args,
+                result: {  // Default is for a syscall to return None
+                    kind: "success",
+                    value: Sk.builtin.none.none$,
+                },
+            };
+            data.set_success = (value) => {
+                data.result = { kind: "success", value };
+            };
+            data.set_failure = (error) => {
+                data.result = { kind: "failure", error };
+            };
+            return data;
+        })();
 
         susp.resume = () => {
             const result = susp.data.result;
