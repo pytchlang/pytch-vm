24e2ef916192b3bb768dada346f067de7b27173a
Ben North <ben@redfrontdoor.org>
2021-05-07 10:33:51 +0100

    Add test skeleton

---
+++ test/pytch/test_ask_answer.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {
+    configure_mocha,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Text input mechanism via "ask and wait for answer"
+
+describe("Ask and wait for answer", () => {
+});
