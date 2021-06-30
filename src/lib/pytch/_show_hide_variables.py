92e0c196d093e2ac42f6c6c0d92eecd40fbf0191
Ben North <ben@redfrontdoor.org>
2021-06-30 16:06:24 +0100

    show_variable(): Add Python wrapper

---
+++ src/lib/pytch/_show_hide_variables.py
@@ -0,0 +1,19 @@
+from .syscalls import _show_object_attribute
+from .project import STAGE_WIDTH, STAGE_HEIGHT
+
+
+DEFAULT_WATCHER_MARGIN = 4
+
+
+def show_variable(obj, attr, *, label=None, top=None, right=None, bottom=None, left=None):
+    "(OBJ, VAR, [...]) Show a watcher for OBJ.VAR"
+
+    if label is None:
+        label = attr
+
+    if left is None and right is None:
+        left = -(STAGE_WIDTH // 2) + DEFAULT_WATCHER_MARGIN
+    if top is None and bottom is None:
+        top = (STAGE_HEIGHT // 2) - DEFAULT_WATCHER_MARGIN
+
+    _show_object_attribute(obj, attr, label, (top, right, bottom, left))
