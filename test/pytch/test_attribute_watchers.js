28df3b7b307bd3571c79f4c7111d77434b0a2570
Ben North <ben@redfrontdoor.org>
2021-06-30 16:10:30 +0100

    Add basic test of watcher functionality

--- test/pytch/test_attribute_watchers.js
+++ test/pytch/test_attribute_watchers.js
@@ -5,6 +5,7 @@ const {
     import_deindented,
     one_frame,
     many_frames,
+    assert_renders_as,
 } = require("./pytch-testing.js");
 configure_mocha();
 
@@ -14,4 +15,29 @@ configure_mocha();
 // Attribute watchers
 
 describe("Attribute watchers", () => {
+    it("can render a Sprite variable", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            class Banana(pytch.Sprite):
+                start_shown = False
+
+                @pytch.when_I_receive("watch-score")
+                def show_score(self):
+                    self.score = 42
+                    pytch.show_variable(self, "score")
+        `);
+
+        // Initially there should be no watchers.
+        many_frames(project, 5);
+        assert_renders_as("start", project, []);
+
+        project.do_synthetic_broadcast("watch-score");
+        one_frame(project);
+
+        assert_renders_as(
+            "post-watch", project,
+            [["RenderAttributeWatcher", "score", "42", 176, null, null, -236]]
+        );
+    });
 });
