34b4d15d8732e1a9b6dbf3ea4de56b7fec47dfe2
Ben North <ben@redfrontdoor.org>
2021-06-30 16:17:16 +0100

    Test adding second watcher

--- test/pytch/test_attribute_watchers.js
+++ test/pytch/test_attribute_watchers.js
@@ -26,6 +26,11 @@ describe("Attribute watchers", () => {
                 def show_score(self):
                     self.score = 42
                     pytch.show_variable(self, "score")
+
+                @pytch.when_I_receive("watch-health")
+                def show_health(self):
+                    self.health = 99
+                    pytch.show_variable(self, "health", right=220)
         `);
 
         // Initially there should be no watchers.
@@ -47,5 +52,25 @@ describe("Attribute watchers", () => {
         one_frame(project);
 
         assert_renders_as("post-second-watch", project, [score_render_instrn]);
+
+        // Watch a second (instance) variable.
+
+        project.do_synthetic_broadcast("watch-health");
+        one_frame(project);
+
+        const health_render_instrn = [
+            "RenderAttributeWatcher", "health", "99", 176, 220, null, null
+        ];
+
+        assert_renders_as("post-watch-health", project,
+                          [score_render_instrn, health_render_instrn]);
+
+        // Re-watch a second (instance) variable; should make no difference.
+
+        project.do_synthetic_broadcast("watch-health");
+        one_frame(project);
+
+        assert_renders_as("post-second-watch-health", project,
+                          [score_render_instrn, health_render_instrn]);
     });
 });
