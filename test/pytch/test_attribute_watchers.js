77d9f2b54d6e7328cd20c87027bebb2923ae2e54
Ben North <ben@redfrontdoor.org>
2021-06-30 15:47:40 +0100

    Add test skeleton

---
+++ test/pytch/test_attribute_watchers.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    one_frame,
+    many_frames,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Attribute watchers
+
+describe("Attribute watchers", () => {
+});
