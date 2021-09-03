0c34ceaf160e1fd72cf4c993a18a4c92b2f44214
Ben North <ben@redfrontdoor.org>
2021-09-03 10:52:12 +0100

    Add test skeleton

---
+++ test/pytch/test-rotation.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    one_frame,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Sprite rotation
+
+describe("Sprite rotation", () => {
+});
