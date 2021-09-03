381db2470b06f2024a0a47c42ee813ac264d32ea
Ben North <ben@redfrontdoor.org>
2021-09-03 11:14:18 +0100

    Add "can turn and point" test

--- test/pytch/test-rotation.js
+++ test/pytch/test-rotation.js
@@ -3,6 +3,8 @@
 const {
     configure_mocha,
     import_deindented,
+    assert_float_close,
+    assert_renders_as,
     one_frame,
 } = require("./pytch-testing.js");
 configure_mocha();
@@ -13,4 +15,38 @@ configure_mocha();
 // Sprite rotation
 
 describe("Sprite rotation", () => {
+    it("can turn and point", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            class Banana(pytch.Sprite):
+                Costumes = ["yellow-banana.png"]
+                @pytch.when_I_receive("turn")
+                def turn(self):
+                    self.turn_degrees(41)
+                @pytch.when_I_receive("point")
+                def point(self):
+                    self.point_degrees(102)
+        `);
+
+        const banana = project.instance_0_by_class_name("Banana");
+
+        const assert_direction = (msg, exp_direction) => {
+            project.do_synthetic_broadcast(msg)
+            one_frame(project);
+            const got_direction = banana.js_attr("direction");
+            assert_float_close(got_direction, exp_direction, 0.001);
+        };
+
+        assert_direction("turn", 41);
+        assert_direction("turn", 82);
+        assert_direction("point", 102);
+        assert_direction("turn", 143);
+
+        assert_renders_as(
+            "final",
+            project,
+            [["RenderImage", 0, 0, 1, "yellow-banana", 143]]
+        );
+    });
 });
