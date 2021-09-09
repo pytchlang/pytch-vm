42262d49d35b0a37958a772269bafa94c60c9493
Ben North <ben@redfrontdoor.org>
2021-09-09 17:02:10 +0100

    Test linear glide function

--- test/pytch/test_glide_easing.js
+++ test/pytch/test_glide_easing.js
@@ -13,4 +13,21 @@ configure_mocha();
 ////////////////////////////////////////////////////////////////////////////////
 
 describe("Glide easing", () => {
+    it("computes linear", async () => {
+        const project = await import_deindented(`
+
+            import pytch
+            import pytch._glide_easing
+
+            cv = pytch._glide_easing.named["linear"]
+            print([cv(t / 22.0) for t in range(0, 23)])
+        `);
+
+        const got_ts = JSON.parse(pytch_stdout.drain_stdout());
+        assert.strictEqual(got_ts.length, 23);
+        got_ts.forEach((got_t, idx) => {
+            const exp_t = idx / 22.0;
+            assert_float_close(got_t, exp_t, 1.0e-15);
+        });
+    });
 });
