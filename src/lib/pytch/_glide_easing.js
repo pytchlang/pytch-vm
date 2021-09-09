d214307bd5879df4135a0220b1dbf8eff79d51e3
Ben North <ben@redfrontdoor.org>
2021-09-09 17:00:37 +0100

    _ease_in_out(): Add

--- src/lib/pytch/_glide_easing.js
+++ src/lib/pytch/_glide_easing.js
@@ -3,5 +3,17 @@ const $builtinmodule = function (name) {
 
     const _linear = new Sk.builtin.func((t) => t);
 
+    const _ease_in_out = new Sk.builtin.func((t) => {
+        const t0 = t.v;
+        const t0_sq = t0 * t0;
+
+        const t_out = (
+            (t0 < 0.5)
+                ? (2.0 * t0_sq)
+                : (-2.0 * t0_sq + 4 * t0 - 1)
+        );
+        return new Sk.builtin.float_(t_out);
+    });
+
     return glide_easing;
 };
