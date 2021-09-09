565f1607fdedef1a8fbaa7cd30fba62a03295ec8
Ben North <ben@redfrontdoor.org>
2021-09-09 17:01:39 +0100

    Add "Glide easing" test skeleton

---
+++ test/pytch/test_glide_easing.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    pytch_stdout,
+    assert,
+    assert_float_close,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+describe("Glide easing", () => {
+});
