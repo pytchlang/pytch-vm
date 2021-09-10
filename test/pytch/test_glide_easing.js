91598029329f046b255faa6bb9a34fe801d2577a
Ben North <ben@redfrontdoor.org>
2021-09-10 08:39:48 +0100

    Remove now-dup'd tests

--- test/pytch/test_glide_easing.js
+++ test/pytch/test_glide_easing.js
@@ -48,46 +48,4 @@ describe("Glide easing", () => {
             });
         });
     });
-
-    it("computes linear", async () => {
-        const project = await import_deindented(`
-
-            import pytch
-            import pytch._glide_easing
-
-            cv = pytch._glide_easing.named["linear"]
-            print([cv(t / 22.0) for t in range(0, 23)])
-        `);
-
-        const got_ts = JSON.parse(pytch_stdout.drain_stdout());
-        assert.strictEqual(got_ts.length, 23);
-        got_ts.forEach((got_t, idx) => {
-            const exp_t = idx / 22.0;
-            assert_float_close(got_t, exp_t, 1.0e-15);
-        });
-    });
-
-    it("computes piecewise-quadratic", async () => {
-        const project = await import_deindented(`
-
-            import pytch
-            import pytch._glide_easing
-
-            cv = pytch._glide_easing.named["ease-in-out"];
-            print([cv(t / 22.0) for t in range(0, 23)])
-        `);
-
-        const exp_ts = [...Array(23).keys()].map(t => t / 22.0).map(t =>
-            ((t < 0.5)
-             ? (2.0 * t * t)
-             : (1.0 - 0.5 * (-2 * t + 2) * (-2 * t + 2))));
-
-        const got_ts = JSON.parse(pytch_stdout.drain_stdout());
-
-        assert.strictEqual(got_ts.length, exp_ts.length);
-        got_ts.forEach((got_t, idx) => {
-            const exp_t = exp_ts[idx];
-            assert_float_close(got_t, exp_t, 1.0e-15);
-        });
-    });
 });
