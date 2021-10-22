dbf363684ca5dcbdfc8c190ab99fced33482b2ef
Ben North <ben@redfrontdoor.org>
2021-10-22 17:21:02 +0100

    Add test file skeleton

---
+++ test/pytch/test_gather_asset_names.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const {
+    configure_mocha,
+    deIndent,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Pre-loading a project to discover what assets it needs.
+
+describe("asset detection", () => {
+});
