f58a8751874fcbf71e3f4a02c101a19cd0c8f568
Ben North <ben@redfrontdoor.org>
2022-04-02 22:03:53 +0100

    Add skeleton file for GPIO tests

---
+++ test/pytch/test_gpios.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    one_frame,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// GPIOs
