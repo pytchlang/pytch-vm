36e83dc3c410b798a55260548e44f6695d4cffaa
Ben North <ben@redfrontdoor.org>
2022-04-03 08:44:43 +0100

    GpioCommand.state: Initialise

--- src/lib/pytch/project.js
+++ src/lib/pytch/project.js
@@ -1659,6 +1659,7 @@ var $builtinmodule = function (name) {
         constructor(operation) {
             this.seqnum = GPIO_next_seqnum();
             this.operation = operation;
+            this.state = { status: "not-sent" };
         }
 
         as_command_obj() {
