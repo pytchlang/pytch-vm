bc8fdc45ba0fc77f7950bf0ffdb8d1f7d7285d18
Ben North <ben@redfrontdoor.org>
2022-04-03 08:53:24 +0100

    Project.set_gpio_level(): Add

--- src/lib/pytch/project.js
+++ src/lib/pytch/project.js
@@ -2013,6 +2013,10 @@ var $builtinmodule = function (name) {
             }
         }
 
+        set_gpio_level(pin, level) {
+            this.enqueue_gpio_command({ kind: "set-output", pin, level });
+        }
+
         enqueue_gpio_command(operation) {
             return this.gpio_command_queue.enqueue_for_sending(operation);
         }
