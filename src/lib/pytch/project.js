49c9ee9ebc7897fe1ad3e863bedc574805009635
Ben North <ben@redfrontdoor.org>
2022-04-03 08:51:45 +0100

    GpioCommandQueue.enqueue_gpio_command(): Add

--- src/lib/pytch/project.js
+++ src/lib/pytch/project.js
@@ -1762,6 +1762,12 @@ var $builtinmodule = function (name) {
             this.unsent_commands = [];
             this.commands_awaiting_response = new Map();
         }
+
+        enqueue_for_sending(operation) {
+            const command = new GpioCommand(operation);
+            this.unsent_commands.push(command);
+        }
+
     }
 
 
