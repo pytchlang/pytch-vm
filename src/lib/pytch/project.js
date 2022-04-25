96c266fb29287f76bcbc154f067895f533020e79
Ben North <ben@redfrontdoor.org>
2022-04-25 09:52:55 +0100

    (Note TODO)

--- src/lib/pytch/project.js
+++ src/lib/pytch/project.js
@@ -2193,6 +2193,7 @@ var $builtinmodule = function (name) {
             this.gpio_command_queue.send_unsent();
 
             const project_state = {
+                // TODO: Add stats on GPIO operations, pin states, etc.?
                 exception_was_raised,
                 maybe_live_question: this.maybe_live_question(),
             };
