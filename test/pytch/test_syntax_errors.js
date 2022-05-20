e0ee8eec5d5ada8493dda0d2b8a6db8b1f78f217
Ben North <ben@redfrontdoor.org>
2022-05-20 11:15:53 +0100

    assertTigerPythonAnalysis(): Add test helper

--- test/pytch/test_syntax_errors.js
+++ test/pytch/test_syntax_errors.js
@@ -10,5 +10,40 @@ configure_mocha();
 
 ////////////////////////////////////////////////////////////////////////////////
 
+const assertTigerPythonAnalysis = (expErrors) => (err) => {
+    assert.ok(err instanceof Sk.pytchsupport.PytchBuildError);
+    assert.equal(err.phase, "import");
+    assert.equal(err.innerError.tp$name, "TigerPythonSyntaxAnalysis");
+
+    const got_errors = err.innerError.syntax_errors;
+
+    assert.equal(
+        got_errors.length,
+        expErrors.length,
+        `expecting ${expErrors.length} error/s but got ${got_errors.length}`
+    );
+
+    expErrors.forEach((expErr, i) => {
+        const gotErr = got_errors[i];
+
+        // The string ": expected" in message confuses Mocha; replace colons.
+        const got_message = gotErr.args.v[0].v.replace(/:/g, "[COLON]");
+        assert.ok(
+            expErr.re.test(got_message),
+            `error[${i}]'s message "${got_message}" did not match /${expErr.re.source}/`
+        );
+
+        const got_line = gotErr.traceback[0].lineno;
+        assert.equal(
+            got_line,
+            expErr.line,
+            `expecting error[${i}] ("${got_message}") to be reported`
+                + ` on line ${expErr.line} but got ${gotErr.line}`
+        );
+    });
+
+    return true;
+}
+
 describe("Syntax errors", () => {
 });
