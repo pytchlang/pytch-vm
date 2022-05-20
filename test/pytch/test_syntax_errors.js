6d2c9fae9b762fcf487e3aa2388a6fe108330630
Ben North <ben@redfrontdoor.org>
2022-05-20 09:35:14 +0100

    Add test file skeleton

---
+++ test/pytch/test_syntax_errors.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const {
+    configure_mocha,
+    import_deindented,
+    assert,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+describe("Syntax errors", () => {
+});
