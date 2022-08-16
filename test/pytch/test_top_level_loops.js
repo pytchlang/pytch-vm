ad03c5c74563cee51bf92420f745f7b0c2aae9a0
Ben North <ben@redfrontdoor.org>
2022-08-16 20:12:55 +0100

    set_max_import_loop_iterations(): Test

--- test/pytch/test_top_level_loops.js
+++ test/pytch/test_top_level_loops.js
@@ -78,6 +78,70 @@ describe("Use of for/while loops at module top-level", () => {
 
     [
         {
+            label: "for",
+            fragment_lines: ["for i in range(n_iters):", "    pass"],
+        },
+        {
+            label: "while",
+            fragment_lines: ["i = 0", "while i < n_iters:", "    i += 1"],
+        },
+    ].forEach(spec => {
+        describe(`control of max top-level loop iters (${spec.label})`, () => {
+            const program_code = (maybe_max, n_iters) => {
+                const set_max_fragment =
+                      ((maybe_max == null)
+                       ? ""
+                       : `pytch.set_max_import_loop_iterations(${maybe_max})`);
+
+                const body = spec.fragment_lines.join("\n                    ");
+                return `
+                    import pytch
+                    ${set_max_fragment}
+                    n_iters = ${n_iters}
+                    ${body}
+                `;
+            };
+
+            const assert_error_fun = assertBuildErrorFun(
+                "import",
+                Sk.builtin.RuntimeError,
+                /loop iterations.*maximum allowed/
+            );
+
+            it("allows iters within limit", async () => {
+                await import_deindented(program_code(null, 1000));
+                // Test is just that this runs OK.
+            });
+
+            it("rejects iters exceeding limit", async () => {
+                const import_project = import_deindented(program_code(null, 1001));
+                await assert.rejects(import_project, assert_error_fun);
+            });
+
+            it("can override max (iters within limit)", async () => {
+                await import_deindented(program_code(1200, 1200));
+                // Test is just that this runs OK.
+            });
+
+            it("can override max (iters exceeding limit)", async () => {
+                const import_project = import_deindented(program_code(200, 201));
+                await assert.rejects(import_project, assert_error_fun);
+            });
+
+            it("can override max to zero (no iters)", async () => {
+                await import_deindented(program_code(0, 0));
+                // Test is just that this runs OK.
+            });
+
+            it("can override max to zero (one iter)", async () => {
+                const import_project = import_deindented(program_code(0, 1));
+                await assert.rejects(import_project, assert_error_fun);
+            });
+        });
+    });
+
+    [
+        {
             label: "string",
             fragment: "'foo'",
             error_type: Sk.builtin.TypeError,
