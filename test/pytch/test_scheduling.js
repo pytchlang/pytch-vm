2b5d9cce7f1e837e22a80339a891bfbeb4f2adbf
Ben North <ben.north@tcd.ie>
2025-03-20 09:39:56 +0000

    wait_seconds(): Restore (simplified) test
    Using inline Python code.

--- test/pytch/test_scheduling.js
+++ test/pytch/test_scheduling.js
@@ -6,6 +6,7 @@ const {
     assert,
     many_frames,
     one_frame,
+    import_deindented,
 } = require("./pytch-testing.js");
 configure_mocha();
 
@@ -186,6 +187,41 @@ describe("scheduling", () => {
             one_frame(project);
             actors.assert_has_steps_and_events(2, 2);
         })});
+
+    it("can pause for a number of seconds", async () => {
+        const project = await import_deindented(`
+            import pytch
+            class Alien(pytch.Sprite):
+                @pytch.when_green_flag_clicked
+                def invade(self):
+                    self.n_steps = 1
+                    pytch.wait_seconds(0.25)
+                    self.n_steps += 1
+        `);
+
+        let alien = project.instance_0_by_class_name("Alien");
+
+        let assert_n_steps = (exp_n_steps => {
+            assert.strictEqual(alien.js_attr("n_steps"), exp_n_steps);
+        });
+
+        project.on_green_flag_clicked();
+        one_frame(project);
+        assert_n_steps(1);
+
+        // The thread is now waiting.  For the next 14 one_frame()
+        // calls it should not be runnable and so nothing should
+        // change.
+        for (let i = 0; i != 14; ++i) {
+            one_frame(project);
+            assert_n_steps(1);
+        }
+
+        // But now it runs again, to completion.
+        one_frame(project);
+        assert_n_steps(2);
+        assert.strictEqual(project.thread_groups.length, 0);
+    });
 
     with_project("py/project/loop_in_module.py", (import_project) => {
         it("yields exactly when meant to", async () => {
