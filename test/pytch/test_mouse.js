805837adcfd27c8fdb811a6016087ca78fa5b7f6
Ben North <ben.north@tcd.ie>
2025-09-18 15:47:14 +0100

    Add unit-test skeleton for mouse tests

---
+++ test/pytch/test_mouse.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const {
+    configure_mocha,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+describe("mouse features", () => {
+});
