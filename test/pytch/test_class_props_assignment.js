6656f74702760f6c5ca7cc681815fb474b5f2f94
Ben North <ben.north@tcd.ie>
2026-07-12 13:35:39 +0100

    Add skeleton file for testing bad prop assignments

---
+++ test/pytch/test_class_props_assignment.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const {
+    configure_mocha,
+} = require("./pytch-testing.js");
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Attempted assignment to, e.g., Banana.size should raise an error.
+
+describe("assignment to class attribute", () => {
+});
