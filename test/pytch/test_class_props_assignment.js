90c04c3658912bf7d4fa4aa42350dcedf2a5fb46
Ben North <ben.north@tcd.ie>
2026-07-12 13:41:02 +0100

    Test error from assignment to class attr

--- test/pytch/test_class_props_assignment.js
+++ test/pytch/test_class_props_assignment.js
@@ -2,6 +2,9 @@
 
 const {
     configure_mocha,
+    one_frame,
+    import_deindented,
+    pytch_errors,
 } = require("./pytch-testing.js");
 configure_mocha();
 
@@ -11,4 +14,58 @@ configure_mocha();
 // Attempted assignment to, e.g., Banana.size should raise an error.
 
 describe("assignment to class attribute", () => {
+    [
+        "Actor.sound_volume",
+        "Sprite.x_position",
+        "Sprite.y_position",
+        "Sprite.direction",
+        "Sprite.size",
+        "Sprite.costume_number",
+        "Sprite.costume_name",
+        "Sprite.distance_to_mouse",
+        "Sprite.touching_mouse",
+        "Stage.backdrop_number",
+        "Stage.backdrop_name",
+    ].forEach(propQName => {
+        const [cls, attr] = propQName.split(".");
+        it(`gives error trying to set ${propQName}`, async () => {
+            const project = await import_deindented(`
+
+                import pytch
+
+                class Background(pytch.Stage):
+                    Backdrops = ["wooden-stage.png"]
+
+                class Banana(pytch.Sprite):
+                    Costumes = ["balloon.png"]
+
+                class Writer(pytch.Sprite):
+                    @pytch.when_I_receive("bad-set-sprite")
+                    def bad_set_sprite(self):
+                        # Value doesn't matter:
+                        Banana.${attr} = 42
+
+                    @pytch.when_I_receive("bad-set-stage")
+                    def bad_set_stage(self):
+                        # Value doesn't matter:
+                        Background.${attr} = 42
+                `);
+
+            const assert_error_on_assign = (msg_suffix, cls_name) => {
+                project.do_synthetic_broadcast(`bad-set-${msg_suffix}`);
+                one_frame(project);
+                pytch_errors.assert_sole_error_matches(
+                    new RegExp(
+                        `property '${attr}' of '${cls_name}'.*not on the class`
+                    )
+                );
+            };
+
+            if (["Sprite", "Actor"].includes(cls))
+                assert_error_on_assign("sprite", "Banana");
+
+            if (["Stage", "Actor"].includes(cls))
+                assert_error_on_assign("stage", "Background");
+        });
+    });
 });
