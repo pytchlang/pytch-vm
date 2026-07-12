b68d268d7d151dd0993e5b42b48b02074f57cdfd
Ben North <ben.north@tcd.ie>
2026-07-12 11:50:19 +0100

    (Reformat)

--- test/pytch/test_position_properties.js
+++ test/pytch/test_position_properties.js
@@ -17,51 +17,52 @@ configure_mocha();
 
 describe("Position properties", () => {
     property_set_mechanism_specs.forEach(spec =>
-    it(`can retrieve coords (${spec.label})`, async () => {
-        const project = await import_deindented(`
+        it(`can retrieve coords (${spec.label})`, async () => {
+            const project = await import_deindented(`
 
-            import pytch
-            class Banana(pytch.Sprite):
-                Costumes = ["yellow-banana.png"]
-                @pytch.when_I_receive("move")
-                def move_around(self):
-                    self.go_to_xy(10, 20)
-                    print(f"{self.x_position} {self.y_position}")
-                    self.set_x(100)
-                    print(f"{self.x_position} {self.y_position}")
-                    self.set_y(-40)
-                    print(f"{self.x_position} {self.y_position}")
-                    self.change_x(10)
-                    print(f"{self.x_position} {self.y_position}")
-                    self.change_y(-30)
-                    print(f"{self.x_position} {self.y_position}")
+                import pytch
+                class Banana(pytch.Sprite):
+                    Costumes = ["yellow-banana.png"]
 
-                @pytch.when_I_receive("move-prop")
-                def move_around_prop(self):
-                    self.go_to_xy(10, 20)
-                    print(f"{self.x_position} {self.y_position}")
-                    self.x_position = 100
-                    print(f"{self.x_position} {self.y_position}")
-                    self.y_position = -40
-                    print(f"{self.x_position} {self.y_position}")
-                    self.x_position += 10
-                    print(f"{self.x_position} {self.y_position}")
-                    self.y_position -= 30
-                    print(f"{self.x_position} {self.y_position}")
-        `);
+                    @pytch.when_I_receive("move")
+                    def move_around(self):
+                        self.go_to_xy(10, 20)
+                        print(f"{self.x_position} {self.y_position}")
+                        self.set_x(100)
+                        print(f"{self.x_position} {self.y_position}")
+                        self.set_y(-40)
+                        print(f"{self.x_position} {self.y_position}")
+                        self.change_x(10)
+                        print(f"{self.x_position} {self.y_position}")
+                        self.change_y(-30)
+                        print(f"{self.x_position} {self.y_position}")
 
-        project.do_synthetic_broadcast(`move${spec.message_suffix}`);
-        one_frame(project);
+                    @pytch.when_I_receive("move-prop")
+                    def move_around_prop(self):
+                        self.go_to_xy(10, 20)
+                        print(f"{self.x_position} {self.y_position}")
+                        self.x_position = 100
+                        print(f"{self.x_position} {self.y_position}")
+                        self.y_position = -40
+                        print(f"{self.x_position} {self.y_position}")
+                        self.x_position += 10
+                        print(f"{self.x_position} {self.y_position}")
+                        self.y_position -= 30
+                        print(f"{self.x_position} {self.y_position}")
+            `);
 
-        const exp_output = [
-            "10 20\n",
-            "100 20\n",
-            "100 -40\n",
-            "110 -40\n",
-            "110 -70\n"
-        ].join("")
+            project.do_synthetic_broadcast(`move${spec.message_suffix}`);
+            one_frame(project);
 
-        assert.equal(pytch_stdout.drain_stdout(), exp_output);
+            const exp_output = [
+                "10 20\n",
+                "100 20\n",
+                "100 -40\n",
+                "110 -40\n",
+                "110 -70\n"
+            ].join("")
+
+            assert.equal(pytch_stdout.drain_stdout(), exp_output);
     }));
 
     it("can retrieve size", async () => {
