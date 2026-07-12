25d2f470bca70d838f2b366a427856ae45cf0d39
Ben North <ben.north@tcd.ie>
2026-07-12 14:39:48 +0100

    Add test skeleton for "no instance-0" error

---
+++ test/pytch/test_missing_instance_0.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const {
+    configure_mocha,
+configure_mocha();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Trying to access a property through the class before the magic
+// original-instance exists should raise an error.
+
+describe("delegated properties without instance-0", () => {
+});
